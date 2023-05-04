8985d2c6c47175b5a95a356ecd735777d375a05b
David Mijailović <mijailovicd00@gmail.com>
2023-05-04 13:25:23 +0200

    Model

---
+++ Levi9.ERP.Domain/Model/Price.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levi9.ERP.Domain.Model
+{
+    public class Price
+    {
+        [Key, Column(Order = 0)]
+        public int ProductId { get; set; }
+        [Key, Column(Order = 1)]
+        public int PriceListId { get; set; }
+        public Product Product { get; set; }
+        public PriceList PriceList { get; set; }
+        public Guid GlobalId { get; set; }
+        [Required]
+        public float PriceValue { get; set; }
+        [StringLength(3), MinLength(3)]
+        public string Currency { get; set; }
+        [StringLength(18), MaxLength(18)]
+        public string LastUpdate { get; set; }
+
+    }
+}
---
+++ Levi9.ERP.Domain/Model/ProductDocument.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levi9.ERP.Domain.Model
+{
+    public class ProductDocument
+    {
+        [Key, Column(Order = 0)]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        [Key, Column(Order = 1)]
+        public int DocumentId { get; set; }
+        public Document Document { get; set; }
+        [Required, Range(0, float.MaxValue)]
+        public float PriceValue { get; set; }
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+        [StringLength(3), MinLength(3)]
+        public string Currency { get; set; }
+    }
+}
